dc574043833fed25d1d2c7c1ed42423ceae19187
mirandalu2020 <yuanling1007@hotmail.com>
2023-03-01 13:52:20 -0800

    modal added

--- src/App.js
+++ src/App.js
@@ -6,9 +6,37 @@ import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 import data from './data.json'
+import Modal from 'react-bootstrap/Modal';
+import './App.css';
 
 //declare class
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    
+      //set state: is modal displaying
+      showModal: false,
+      beastImgUrl: '',
+      beastDescription:'',
+    }
+  }
+
+  //each updates the state of the modal
+  handleCloseModal = () => {
+    this.setState({
+      showModal: false
+    })
+  }
+
+  SelectedBeast = (imageUrl,description) => {
+    this.setState({
+      showModal: true,
+      beastImg: imageUrl,
+      beastDescription: description,
+
+    });
+  }
 
   render() {
 
@@ -17,11 +45,24 @@ class App extends React.Component {
   // the fractional marginal unit to wrap components inside -- wrap JSX inside parents
     <>
       <Header/>
-      <Main data = {data}/> 
+      <p onClick={this.SelectedBeast}>Open Modal</p>
+
+      <Main 
+      data = {data} 
+      SelectedBeast = {this.SelectedBeast}/>
+
       <Footer/>
+
+      <Modal 
+      show={this.state.showModal} 
+      onHide = {this.handleCloseModal}>
+      <img src={this.state.beastImg} alt=" "/>
+      <p>{this.state.beastDescription}</p>          
+      
+      </Modal>
+
     </>
     )
-    
   }
 };
 
